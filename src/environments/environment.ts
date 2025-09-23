b9f78a81bc106e9f8165eafefa9668aebb800ae9
KleberMera <klebermera2016@gmail.com>
2025-09-23 16:55:25 -0500

    fixed producction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production : false,
     // uri: 'https://www.lalibertad.gob.ec/wsLibertad/api/'
     // uri: 'https://120.40.69.189/wsLibertad/api/'
-    //uri: 'https://www.lalibertad.gob.ec/wsLibertad_v2/api/' //Producción
+    uri: 'https://www.lalibertad.gob.ec/wsLibertad_v2/api/' //Producción
     // uri: 'http://120.40.73.73:8080/wsLibertad_v2/api/' //Desarrollo4
-    uri: 'http://localhost:8080/wsLibertad_v2/api/'
+    //uri: 'http://localhost:8080/wsLibertad_v2/api/'
 }
